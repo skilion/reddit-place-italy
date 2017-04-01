9cb3b87950e89b3215dcd655e730d238bfa288e7
skilion <andrea.9940@gmail.com>
2017-04-02 00:58:21 +0200

    tmp code to protect the flag white separated file to download the board

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if (users.test) {
 	process.exit(1);
 }
 
-let username, password;
+let username, password, modhash;
 for (var key in users) {
 	username = key;
 	password = users[username];
@@ -23,56 +23,48 @@ r.login(username, password, function(err, httpResponse, body) {
 		console.log('login error');
 		return;
 	}
-	/* modhash = body.json.data.modhash;
-	/r.draw(10, 0, 0, modhash, function(err, httpResponse, body) {
-		console.log(body);
-	});*/
-	r.pixel(10, 0, function(err, httpResponse, body) {
-		console.log(body);
-	});
+	modhash = body.json.data.modhash;
+	loop();
 });
 
-
-r_board(function(err, httpResponse, body) {
-	bitmap2PNG(body).pack().pipe(fs.createWriteStream('board.png'));
-});
-
-const colors = [
-	[255, 255, 255],
-	[228, 228, 228],
-	[136, 136, 136],
-	[34, 34, 34],
-	[255, 167, 209],
-	[229, 0, 0],
-	[229, 149, 0],
-	[160, 106, 66],
-	[229, 217, 0],
-	[148, 224, 68],
-	[2, 190, 1],
-	[0, 211, 221],
-	[0, 131, 199],
-	[0, 0, 234],
-	[207, 110, 228],
-	[130, 0, 128]
-];
-
-function bitmap2PNG(bitmap) {
-	let img = new PNG({ width: 1000, height: 1000, inputHasAlpha: false });
-	let i = 0;
-	for (let y = 0; y < 1000; y++) {
-		for (let x = 0; x < 500; x++) {
-			let b = bitmap[i];
-			let c1 = colors[b >> 4];
-			let c2 = colors[b & 0xF];
-			let idx = (x * 2 + y * 1000) * 3;
-			img.data[idx + 0] = c1[0];
-			img.data[idx + 1] = c1[1];
-			img.data[idx + 2] = c1[2];
-			img.data[idx + 3] = c2[0];
-			img.data[idx + 4] = c2[1];
-			img.data[idx + 5] = c2[2];
-			i++;
+function loop() {
+	r.time(function(err, httpResponse, body) {
+		body = JSON.parse(body);
+		if (body.wait_seconds == 0) {
+			var d = new Date();
+			console.log(d);
+			console.log('Finding pixel...');
+			findPixel();
+			return;
 		}
-	}
-	return img;
+		setTimeout(loop, 60000);
+	});
+}
+
+function findPixel() {
+	let x = getRandomInt(119, 129);
+	let y = getRandomInt(326, 409);
+	r.pixel(x, y, function(err, httpResponse, body) {
+		body = JSON.parse(body);
+		if (body.color != 0) {
+			setPixel(x, y);
+			return;
+		}
+		setTimeout(findPixel, 5);
+	});
+}
+
+function setPixel(x, y) {
+	r.draw(x, y, 0, modhash, function(err, httpResponse, body) {
+		let res = JSON.parse(body);
+		if ("json" in res) {
+			console.log('r.draw failed ' + body);
+		}
+		console.log('Pixel set ' + x + ' ' + y);
+		setTimeout(loop, res.wait_seconds * 1000);
+	});
+}
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
 }
