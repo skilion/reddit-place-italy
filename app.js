c9c8d1d4ab6d9aa360cc1c358daf0af7cc66c267
skilion <andrea.9940@gmail.com>
2017-04-02 01:54:06 +0200

    use r.board to avoid throttling with r.pixel

--- app.js
+++ app.js
@@ -42,16 +42,34 @@ function loop() {
 }
 
 function findPixel() {
-	let x = getRandomInt(119, 129);
-	let y = getRandomInt(326, 409);
-	r.pixel(x, y, function(err, httpResponse, body) {
-		body = JSON.parse(body);
-		if (body.color != 0) {
+	r.board(function(err, httpResponse, body) {
+		let bitmap = new Array(1000 * 1000);
+		let i = 4;
+		for (let y = 0; y < 1000; y++) {
+			for (let x = 0; x < 500; x++) {
+				let b = body[i];
+				let p1 = b >> 4;
+				let p2 = b & 0xF;
+				let idx = x * 2 + y * 1000;
+				bitmap[idx] = p1;
+				bitmap[idx + 1] = p2;
+				i++;
+			}
+		}
+		findPixel2(bitmap);
+	});
+}
+
+function findPixel2(bitmap) {
+	for (let i = 0; i < 1000000; i++) {
+		let x = getRandomInt(119, 129);
+		let y = getRandomInt(326, 407);
+		if (bitmap[x + y * 1000] != 0) {
 			setPixel(x, y);
 			return;
 		}
-		setTimeout(findPixel, 5);
-	});
+	}
+	setTimeout(findPixel, 5);
 }
 
 function setPixel(x, y) {
