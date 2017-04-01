3e215fccbf866214f3df22ec7a9f11eb1e246dd4
skilion <andrea.9940@gmail.com>
2017-04-01 23:52:07 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+let fs = require('fs');
+let PNG = require('pngjs').PNG;
+var jsonfile = require('jsonfile');
+
+let r = require('./r');
+
+var users = jsonfile.readFileSync("users.json");
+if (users.test) {
+	console.log('Put your credentials in "users.json"');
+	process.exit(1);
+}
+
+let username, password;
+for (var key in users) {
+	username = key;
+	password = users[username];
+}
+
+
+r.login(username, password, function(err, httpResponse, body) {
+	body = JSON.parse(body);
+	if (body.json.errors.length) {
+		console.log('login error');
+		return;
+	}
+	/* modhash = body.json.data.modhash;
+	/r.draw(10, 0, 0, modhash, function(err, httpResponse, body) {
+		console.log(body);
+	});*/
+	r.pixel(10, 0, function(err, httpResponse, body) {
+		console.log(body);
+	});
+});
+
+
+r_board(function(err, httpResponse, body) {
+	bitmap2PNG(body).pack().pipe(fs.createWriteStream('board.png'));
+});
+
+const colors = [
+	[255, 255, 255],
+	[228, 228, 228],
+	[136, 136, 136],
+	[34, 34, 34],
+	[255, 167, 209],
+	[229, 0, 0],
+	[229, 149, 0],
+	[160, 106, 66],
+	[229, 217, 0],
+	[148, 224, 68],
+	[2, 190, 1],
+	[0, 211, 221],
+	[0, 131, 199],
+	[0, 0, 234],
+	[207, 110, 228],
+	[130, 0, 128]
+];
+
+function bitmap2PNG(bitmap) {
+	let img = new PNG({ width: 1000, height: 1000, inputHasAlpha: false });
+	let i = 0;
+	for (let y = 0; y < 1000; y++) {
+		for (let x = 0; x < 500; x++) {
+			let b = bitmap[i];
+			let c1 = colors[b >> 4];
+			let c2 = colors[b & 0xF];
+			let idx = (x * 2 + y * 1000) * 3;
+			img.data[idx + 0] = c1[0];
+			img.data[idx + 1] = c1[1];
+			img.data[idx + 2] = c1[2];
+			img.data[idx + 3] = c2[0];
+			img.data[idx + 4] = c2[1];
+			img.data[idx + 5] = c2[2];
+			i++;
+		}
+	}
+	return img;
+}
---
+++ r.js
@@ -0,0 +1,48 @@
+/////////////////////////
+// Reddit /r/place API //
+/////////////////////////
+
+let request = require('request');
+request = request.defaults({ baseUrl: 'https://www.reddit.com/api/', jar: true });
+
+// perform login
+exports.login = function(username, password, callback) {
+	let options = {
+		url: 'login',
+		form: { user: username, passwd: password, api_type: "json" }
+	}
+	request.post(
+		options,
+		callback
+	);
+}
+
+// get a pixel color
+exports.pixel = function(x, y, callback) {
+	request.get(
+		'place/pixel.json?x=' + x + '&y=' + y,
+		callback
+	);
+}
+
+// set a pixel color
+exports.draw = function(x, y, color, modhash, callback) {
+	let options = {
+		url: 'place/draw.json',
+		form: { x: x, y: y, color: color },
+		headers: { 'X-Modhash': modhash }
+	}
+	request.post(
+		options,
+		callback
+	);
+}
+
+// download the board bitmap
+exports.board = function(callback) {
+	let options = {
+		url: 'place/board-bitmap',
+		encoding: null
+	}
+	request.get(options, callback);
+}
